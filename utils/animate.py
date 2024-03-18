00d4c9ba42b9cd2f8954a10cc56b4c26854b18d5
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2024-03-17 21:15:39 -0400

    added other gif

--- utils/animate.py
+++ utils/animate.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 EX_IDX = 0
 
 
-with np.load(r'results/deep-cosmos-821-test.npz') as data:
+with np.load(r'results/comic-durian-847-test.npz') as data:
     predictions = data['predictions'][EX_IDX, ...]
     actuals = data['actuals'][EX_IDX, ...]
 
@@ -16,7 +16,7 @@ vmin = min(actuals.min(), predictions.min())
 vmax = min(actuals.max(), predictions.max())
 
 fig, axs = plt.subplots(1,3, figsize=(14,8))
-fig.suptitle('Conv LSTM Model')
+fig.suptitle('FNO Model')
 actuals_im = axs[0].imshow(actuals[..., 0], vmin=vmin, vmax=vmax, cmap='rainbow', origin="lower",)
 preds_im = axs[1].imshow(predictions[..., 0], vmin=vmin, vmax=vmax, cmap='rainbow', origin="lower",)
 error_im = axs[2].imshow(error[..., 0], vmin=vmin, vmax=vmax, cmap='rainbow', origin="lower",)
@@ -35,6 +35,6 @@ cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])  # [left, bottom, width, height]
 fig.colorbar(actuals_im, cax=cbar_ax, fraction=0.046, pad=0.04)
 
 ani = FuncAnimation(fig, update, frames=predictions.shape[-1])
-ani.save('conv_lstm_ns_e-3.gif', fps=3)
+ani.save('fno_ns_e-3.gif', fps=3)
 # fig.colorbar()
 plt.show()
