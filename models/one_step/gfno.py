b3d807996604119353fb0aad2ca293b0426cf789
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2024-04-21 22:07:43 -0400

    fixed gfno

--- models/one_step/gfno.py
+++ models/one_step/gfno.py
@@ -6,7 +6,7 @@ from einops import rearrange
 import math
 
 from models.layers.base_layers import MLP
-from models.layers.fourier_blocks import TokenFNOBranch
+from models.layers.fourier_blocks_dim_last import TokenFNOBranch
 from models.layers.graph_blocks import GNOBlockSingleConv, GNOBlockSingleConvAddNodesToEdge, CombineInitAndEdges
 
 class GFNOBlock(nn.Module):
@@ -53,12 +53,12 @@ class GFNO(nn.Module):
         self.edge_dims = 5
         self.graph_passes = config['GRAPH_PASSES']
         # check how we will treat the edges
-        self.add_nodes_to_edge = config.get("ADD_NODES_TO_EDGE", False)
-        self.add_init_to_edge = config.get("ADD_INIT_TO_EDGE", False)
-        assert (self.add_nodes_to_edge and self.add_init_to_edge) == False
-        if(self.add_init_to_edge):
-            self.edge_dims += 2
-            self.edge_attr_updater = CombineInitAndEdges()
+        self.add_nodes_to_edge = config.get("ADD_NODES_TO_EDGE", False) and False
+        self.add_init_to_edge = config.get("ADD_INIT_TO_EDGE", False) and False
+        # assert (self.add_nodes_to_edge and self.add_init_to_edge) == False
+        # if(self.add_init_to_edge):
+        #     self.edge_dims += 2
+        #     self.edge_attr_updater = CombineInitAndEdges()
         # setup layers
         self.project = MLP(self.in_dims, self.latent_dims, self.latent_dims//2)
         self.decode = MLP(self.latent_dims, 1, self.latent_dims//2)
@@ -67,12 +67,10 @@ class GFNO(nn.Module):
             self.blocks.append(GFNOBlock(self.latent_dims, self.modes[0], self.modes[1], self.kernel_dims, self.edge_dims, self.graph_passes, self.add_init_to_edge, self.add_nodes_to_edge, padding_mode=self.padding_mode))
 
     def forward(self, nodes, grid, edge_index, edge_attr, batch_size, image_size):
-        if(self.add_init_to_edge):
-            edge_attr = self.edge_attr_updater(edge_index, edge_attr, nodes[..., -1:])
+        # if(self.add_init_to_edge):
+        #     edge_attr = self.edge_attr_updater(edge_index, edge_attr, nodes[..., -1:])
         # add the grid to the data
         nodes = torch.cat((nodes, grid), dim=-1)
-        B, C = nodes.shape
-        assert C == self.in_dims and B == batch_size * image_size[0] * image_size[1]
         # project the data
         nodes = self.project(nodes)
         # go thorugh the blocks
