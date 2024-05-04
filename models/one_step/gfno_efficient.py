1587e4a3228897e570e80ec63018ed34fc0fd8a9
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2024-05-04 16:46:21 -0400

    gfno eff

--- models/one_step/gfno_efficient.py
+++ models/one_step/gfno_efficient.py
@@ -10,17 +10,18 @@ from models.layers.fourier_blocks_dim_last import TokenFNOBranch
 from models.layers.graph_blocks import GNOBlockEfficient
 
 class GFNOBlock(nn.Module):
-    def __init__(self, latent_dims, modes1, modes2, edge_dims:int, mlp_ratio:int=None):
+    def __init__(self, latent_dims, modes1, modes2, edge_dims:int, graph_passes:int, mlp_ratio:int=None):
         super().__init__()
         # vars
         self.latent_dims = latent_dims
         self.modes1 = modes1
         self.modes2 = modes2
         self.edge_dims = edge_dims
+        self.graph_passes = graph_passes
         self.mlp_ratio = mlp_ratio
         # layers
         self.fno_block = TokenFNOBranch(self.latent_dims, self.modes1, self.modes2, mlp_ratio=mlp_ratio)
-        self.gno_block = GNOBlockEfficient(self.latent_dims, self.edge_dims, mlp_ratio=mlp_ratio)
+        self.gno_block = GNOBlockEfficient(self.latent_dims, self.edge_dims, self.graph_passes, mlp_ratio=mlp_ratio)
         self.norm = nn.LayerNorm(self.latent_dims)
 
     def forward(self, nodes, edge_index, edge_attrs, batch_size, image_size):
@@ -40,10 +41,11 @@ class GFNOEfficient(nn.Module):
         self.modes = (config['MODES1'], config['MODES2'])
         # graph based information
         self.edge_dims = 5
+        self.graph_passes = config['GRAPH_PASSES']
         # setup layers
         self.project = MLP(self.in_dims, self.latent_dims, self.latent_dims//2)
         self.decode = MLP(self.latent_dims, 1, self.latent_dims//2)
-        self.blocks = nn.ModuleList([GFNOBlock(self.latent_dims, self.modes[0], self.modes[1], self.edge_dims) for _ in range(self.depth)])   
+        self.blocks = nn.ModuleList([GFNOBlock(self.latent_dims, self.modes[0], self.modes[1], self.edge_dims, self.graph_passes) for _ in range(self.depth)])   
 
     def forward(self, nodes, grid, edge_index, edge_attr, batch_size, image_size):
         # add the grid to the data
